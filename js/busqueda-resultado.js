cd65ce3fd5e02ae30f3d61180d287b8088cd2660
MateoSilveri <msilveri@udesa.edu.ar>
2022-11-11 17:09:59 -0300

    resultadoform

---
+++ js/busqueda-resultado.js
@@ -0,0 +1,15 @@
+let form = document.querySelector('#form')
+let input = document.querySelector('#busquedaPalabra')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (input.value == "") {
+        alert('No puedes enviar un form vacio')
+    } else if(input.value.length < 3){
+        alert('Debes escribir mas de 3 caracteres')
+    } else {
+        this.submit()
+    }
+
+})
