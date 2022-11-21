d5aba2bcc2f0f3cce8e1dc6462d71942044c08ad
MartinYunes <myunesmarangoni@udesa.edu.ar>
2022-11-21 17:38:32 -0300

    generos y generos

--- js/genres.js
+++ js/genres.js
@@ -6,31 +6,79 @@ let Pelisid = qsObjLit.get('genres_id')
 
 let Seriesid = qsObjLit.get('genres_id')
 
-let urlDetalleGenresPeliculas = `https://api.themoviedb.org/3/genre/movie/${Pelisid}list?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
-let urlDetalleGenresSeries = `https://api.themoviedb.org/3/genre/tv/${Seriesid}list?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US`
+let urlpopmovies = `https://api.themoviedb.org/3/movie/popular?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
+let urlpopseries = `https://api.themoviedb.org/3/tv/popular?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
 let tituloGenero = document.querySelector('.tituloGenero')
 
-fetch(urlDetalleGenresPeliculas).then(function (response) {
-    return response.json()
-}).then(function (data) {
-    let arrayDeResultadosPelis = data.results   
-    console.log(arrayDeResultadosPelis);                           
-    let detalleGenresPelis = ''
-    let genresPeliculas = document.querySelector('#genrespeliculas')
+fetch(urlpopmovies)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let arraypopmovies= data.results;
+        let section1 = document.querySelector('#pelirecomendadasid');
+        let popmovies=[];
 
 
-    //2 Recorro la información de la api y la organizo para mostarla en el html
-    for(let i=0; i<5; i++){
-        //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
-        detalleGenresPelis += `<article class="peliculaoserieinfo">
-                                <p class="nombrepeli-serie">${arrayDeResultadosPelis[i].title}</p>
-                                <img src='img src='https://image.tmdb.org/t/p/w500/${data.poster_path}'  alt="${data.title}" class="poster">
-                                <a href="./detail-movie.html?id=${arrayDeResultadosPelis[i].id}" class="linkdetalle">Ver más</a>
-                            </article>`
-    }
-    //Con toda la estructura html completa ahora la paso al DOM
-    genresPeliculas.innerHTML = detalleGenresPelis;
-}).catch(function (error) {
-    console.log('el error es' + error);
+        for (let i=0; i<5; i++){
+
+            let title= arraypopmovies[i].title
+            let imagen = arraypopmovies[i].poster_path
+            let fecha = arraypopmovies[i].release_date
+            let id= arraypopmovies[i].id
+
+           popmovies += 
+            `<div class="peliculaoserieinfo">
+            <a href="./detail-movie.html?id=${id}"><img  class="peli" src="https://image.tmdb.org/t/p/w500/${imagen}" height="280px" width="220px"></a>
+            <h3 class="nombres">${title}</h3>
+            <li class="fechas">${fecha}</li>
+            </div>`
+        }
+
+        section1.innerHTML= popmovies;
+
+    })
+
+    .catch(function(error){
+        console.log(error);
+    })
+
+
+    fetch(urlpopseries)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let arraypopseries= data.results;
+        let section2 = document.querySelector('#seriesrecomendadasid');
+        let popseries=[];
+
+        for (let i=0; i<5; i++){
+            
+            let title= arraypopseries[i].original_name
+            let imagen = arraypopseries[i].poster_path
+            let fecha = arraypopseries[i].first_air_date
+            let id= arraypopseries[i].id
+
+            popseries += 
+            `<div class="peliculaoserieinfo">
+            <a href="./detail-serie.html?id=${id}"><img  class="peli" src="https://image.tmdb.org/t/p/w500/${imagen}" height="280px" width="220px"></a>
+            <h3 class="nombres">${title}</h3>
+            <li class="fechas">${fecha}</li>
+            </div>`
+        }
+
+        section2.innerHTML = popseries;
+
+    })
+
+    .catch(function(error){
+        console.log(error);
+    })
+
+
     
-})
