e86d10b955cf1c7b768bec673843f9bdc74e36de
MateoSilveri <msilveri@udesa.edu.ar>
2022-11-22 17:07:16 -0300

    form 3len

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -86,3 +86,21 @@ fetch(providers_url)
 })
 
 
+let form = document.querySelector('#form')
+let input = document.querySelector('#busquedaPalabra')
+
+let seccionPelis = document.querySelector('#peliFavorita');
+let seccionTv = document.querySelector('#serieFavorita');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (input.value == "") {
+        alert('No puedes enviar un form vacio')
+    } else if(input.value.length < 3){
+        alert('Debes escribir mas de 3 caracteres')
+    } else {
+        this.submit()
+    }
+
+})
--- js/index.js
+++ js/index.js
@@ -112,3 +112,22 @@ fetch(urlpopseries)
     .catch(function(error){
         console.log(error);
     })
+
+    let form = document.querySelector('#form')
+    let input = document.querySelector('#busquedaPalabra')
+    
+    let seccionPelis = document.querySelector('#peliFavorita');
+    let seccionTv = document.querySelector('#serieFavorita');
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+    
+        if (input.value == "") {
+            alert('No puedes enviar un form vacio')
+        } else if(input.value.length < 3){
+            alert('Debes escribir mas de 3 caracteres')
+        } else {
+            this.submit()
+        }
+    
+    })
