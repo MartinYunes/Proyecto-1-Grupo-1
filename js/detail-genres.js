f0962cb035e08a96b40264d39020c619f077add7
MateoSilveri <msilveri@udesa.edu.ar>
2022-11-23 18:18:18 -0300

    cambios estructura

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,69 +1,70 @@
+
 let qs = location.search;
 let qsObjLit = new URLSearchParams(qs);
 
-let idPeli = qsObjLit.get('id_genero_pelis')
+let idPeli = qsObjLit.get('id_generopelicula')
 
 
-let idSeries = qsObjLit.get('id_genero_series')
+let idSeries = qsObjLit.get('id_generoserie')
 
-let urlDetalleGeneroPeliculas = `https://api.themoviedb.org/3/discover/movie?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idPeli}&with_watch_monetization_types=flatrate`
-let urlDetalleGeneroSeries = `https://api.themoviedb.org/3/discover/tv?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${idSeries}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+let detailPeliGeneros = `https://api.themoviedb.org/3/discover/movie?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idPeli}&with_watch_monetization_types=flatrate`
+let detailSerieGeneros = `https://api.themoviedb.org/3/discover/tv?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${idSeries}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
 
 let tituloGenero = document.querySelector('.tituloGenero')
 
-fetch(urlDetalleGeneroPeliculas).then(function (response) {
+fetch(detailPeliGeneros).then(function (response) {
     return response.json()
 }).then(function (data) {
-    let arrayDeResultadosPelis = data.results   
-    console.log(arrayDeResultadosPelis);                           
-    let detalleGenerosPelis = ''
+    let listapelis = data.results   
+    console.log(listapelis);                           
+    let detallepeli = ''
     let generosPeliculas = document.querySelector('#genrespeliculas')
 
 
     
     for(let i=0; i<5; i++){
        
-        detalleGenerosPelis += `<article class="peliculaoserieinfo">
-                                <p class="nombrepeli-serie">${arrayDeResultadosPelis[i].title}</p>
-                                <img src='https://image.tmdb.org/t/p/w500/${arrayDeResultadosPelis[i].poster_path}'  alt="" class="tapapelicula">
-                                <a href="./detail-movie.html?id=${arrayDeResultadosPelis[i].id}" class="linkadetalle">Ver más</a>
+        detallepeli += `<article class="peliculaoserieinfo">
+                                <p class="nombrepeli-serie">${listapelis[i].title}</p>
+                                <img src='https://image.tmdb.org/t/p/w500/${listapelis[i].poster_path}'  alt="" class="tapapelicula">
+                                <a href="./detail-movie.html?id=${listapelis[i].id}" class="linkadetalle">Ver más</a>
                             </article>`
     }
    
-    generosPeliculas.innerHTML = detalleGenerosPelis;
+    generosPeliculas.innerHTML = detallepeli;
 }).catch(function (error) {
     console.log('el error es' + error);
     
 })
 
-fetch(urlDetalleGeneroSeries).then(function (response) {
+fetch(detailSerieGeneros).then(function (response) {
     return response.json()
 }).then(function (data) {
-    let arrayDeResultadosSeries = data.results
-    console.log(arrayDeResultadosSeries);
-    let detalleGenerosSeries = ''
+    let listaseries = data.results
+    console.log(listaseries);
+    let detalleSeries = ''
     let generosSeries = document.querySelector('#genresseries')
 
     for(let i=0; i<5; i++){
        
-        detalleGenerosSeries += 
+        detalleSeries += 
         `<div class="peliculaoserieinfo">
-            <p class="nombrepeli-serie">${arrayDeResultadosSeries[i].name}</p>
-            <img src='https://image.tmdb.org/t/p/w500/${arrayDeResultadosSeries[i].poster_path}'  alt="" class="tapapelicula">
-            <a href="./detail-serie.html?id=${arrayDeResultadosSeries[i].id}" class="linkadetalle">Ver más</a>
+            <p class="nombrepeli-serie">${listaseries[i].name}</p>
+            <img src='https://image.tmdb.org/t/p/w500/${listaseries[i].poster_path}'  alt="">
+            <a href="./detail-serie.html?id=${listaseries[i].id}" class="linkadetalle">Ver más</a>
         </div>`
     }
    
-    generosSeries.innerHTML = detalleGenerosSeries;
+    generosSeries.innerHTML = detalleSeries;
 }).catch(function (error) {
     return error
 })
 
-let generosPeliculas = document.querySelector('#genrespeliculas')
-let generosSeries = document.querySelector('#genresseries')
+let genresPeli = document.querySelector('#genrespeliculas')
+let genresSeries = document.querySelector('#genresseries')
 
 if (qs.search("series") != -1) {
-    generosPeliculas.style.display = "none"
+    genresPeli.style.display = "none"
 } else {
-    generosSeries.style.display = "none"
+    genresSeries.style.display = "none"
 }
